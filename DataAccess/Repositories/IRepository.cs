5943efbb7a09fc0e4a0de4030cfdd3c957f81908
kutukov2000 <kutukov16.2005@gmail.com>
2023-11-05 14:29:58 +0200

    Create repository interface and class

---
+++ DataAccess/Repositories/IRepository.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq.Expressions;
+
+namespace DataAccess.Repositories
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        void Save();
+
+        IEnumerable<TEntity> Get(
+            Expression<Func<TEntity, bool>> filter = null,
+            Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
+            string includeProperties = "");
+
+        TEntity GetByID(object id);
+
+        void Insert(TEntity entity);
+
+        void Delete(object id);
+
+        void Delete(TEntity entityToDelete);
+
+        void Update(TEntity entityToUpdate);
+    }
+}
